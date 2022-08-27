8a7249b21b86a6614470dbd5e11dffd9f514c222
digihunch <50001769+digihunch@users.noreply.github.com>
2022-08-27 11:40:28 -0400

    Update restart-minikube.sh
    restart-minikube.sh use a separate non-default profile
    use docker as driver instead of hyperkit (because I use M1 processor now....)

--- minikube/restart-minikube.sh
+++ minikube/restart-minikube.sh
@@ -1,19 +1,34 @@
 #! /bin/bash
 
-KUBE_VERSION=v1.23.4
+# Check version history https://kubernetes.io/releases/
+KUBE_VERSION=v1.24.3
+
+# node size of the cluster to create
 NODE_COUNT=3
+
+# containerd is the standard CRI 
 CRI=containerd
 
+# M1 processor, install Docker desktop and use docker as driver
+# Intel processor, use docker driver or hyperkit driver (requiring brew install hyperkit)
+# Currently, hyperkit does not support M1 processor
+DRIVER=docker    
 
+# Custom minikube profile
+PROFILE_NAME=rqc
 
-minikube stop
-minikube delete
-minikube start --memory=12288 --cpus=6 --kubernetes-version=$KUBE_VERSION --nodes $NODE_COUNT --container-runtime=$CRI --driver=hyperkit --disk-size=150g
-minikube addons enable metallb
-minikube addons enable metrics-server
-MINIKUBE_IP=$(minikube ip)
+# if the minikube profile already exists, then stop and delete the cluster first
+minikube profile list --output=table | awk -F'[| ]' '{print $3}' | awk '!/Profile|---------/' | grep ^$PROFILE_NAME$ && minikube stop --profile=$PROFILE_NAME && minikube delete --profile=$PROFILE_NAME
+
+# start the cluster under the specified profile name
+minikube start --profile=$PROFILE_NAME --memory=12288 --cpus=3 --kubernetes-version=$KUBE_VERSION --nodes $NODE_COUNT --container-runtime=$CRI --driver=$DRIVER --disk-size=150g
+minikube addons enable metallb --profile=$PROFILE_NAME
+minikube addons enable metrics-server --profile=$PROFILE_NAME
+
+# interactively configure load balancer IP range
+MINIKUBE_IP=$(minikube ip --profile=$PROFILE_NAME)
 expect << _EOF_
-spawn minikube addons configure metallb
+spawn minikube addons configure metallb --profile=$PROFILE_NAME
 expect "Enter Load Balancer Start IP:" { send "${MINIKUBE_IP%.*}.16\\r" }
 expect "Enter Load Balancer End IP:" { send "${MINIKUBE_IP%.*}.23\\r" }
 expect eof
